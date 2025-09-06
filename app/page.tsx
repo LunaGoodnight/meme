7570fac49d841f677443c1502210073653497fa4
Luna <javaspringtea@gmail.com>
2025-09-07 01:38:53 +0800

    add type

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default async function Home() {
-  const data = await fetch('https://api.vercel.app/blog')
+  const data = await fetch('https://api.meme.vividcats.org/api/memes')
   const posts = await data.json()
   return (
       <ul>
