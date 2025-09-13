9aaaf2acac0824ce20f1044ae7fed86989acb84b
Luna <javaspringtea@gmail.com>
2025-09-14 01:40:14 +0800

    add display

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
 import {MemeUploader} from "@/app/UploadImage";
 import {MemeDisplay} from "@/app/MemeDisplay";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001/api';
 
 export default async function Home() {
-
-
-    const data = await fetch('https://api.meme.vividcats.org/api/memes')
-    const posts = await data.json()
-
-
     return (
         <div>
             <div className='p-20'>
