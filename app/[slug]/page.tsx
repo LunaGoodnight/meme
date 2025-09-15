f374d237333b96aacacddb8783fc9d709b7a1945
Luna <javaspringtea@gmail.com>
2025-09-15 21:16:00 +0800

    update layout

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -8,9 +8,11 @@ export default async function Page({
     const data = await fetch(`https://api.meme.vividcats.org/api/memes/${slug}`)
     const posts = await data.json()
 
+    const img = 'https://apl-hamivideo.cdn.hinet.net/HamiVideo/getHamiVideoImg.php?imageId=sourceImages@4048100287_o_20250805191400.jpg'
+    const webp = 'https://i.imgur.com/aCx5BYJ_d.webp'
     if (posts) {
-        return <div className='flex justify-center items-center min-h-screen w-full p-4'>
-            <img className='w-full max-w-2xl h-auto object-contain md:w-auto md:max-h-[80vh]'
+        return <div className='flex justify-center items-center min-h-screen p-4'>
+            <img className='w-full h-auto object-contain md:max-w-none md:max-h-none md:w-auto'
                  src={posts?.imageUrl}
                  alt=""/>
         </div>
