c88f8a791f0119aa178b86dc7097c27a018ab46a
Luna <javaspringtea@gmail.com>
2025-09-15 21:08:21 +0800

    update layout

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -9,8 +9,10 @@ export default async function Page({
     const posts = await data.json()
 
     if (posts) {
-        return <div>
-            <img src={posts?.imageUrl} alt=""/>
+        return <div className='flex justify-center items-center min-h-screen w-full p-4'>
+            <img className='w-full max-w-2xl h-auto object-contain md:w-auto md:max-h-[80vh]'
+                 src={posts?.imageUrl}
+                 alt=""/>
         </div>
     }
     return <div>No such image</div>
